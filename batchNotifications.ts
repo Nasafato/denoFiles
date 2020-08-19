ae0e88bb371f274282de4e009d6cc44ba98936ca
nasafato <alanjgou@gmail.com>
2020-08-19 16:36:17 -0700

    Add deno files

--- batchNotifications.ts
+++ batchNotifications.ts
@@ -1,6 +1,3 @@
-import { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
-import { delay } from "https://deno.land/std@0.65.0/async/delay.ts";
-
 export interface BatchRecord {
   batchKey: string;
   events: Record<string, any>[];
@@ -45,55 +42,3 @@ function setBatchTrigger(batchKey: string, waitForMs: number) {
     batchStore[batchKey] = null;
   }, waitForMs);
 }
-
-interface Update {
-  id: string;
-  data: {
-    [key: string]: any;
-  };
-}
-
-Deno.test("should be notified with two batches", async () => {
-  const recordedBatches: any[] = [];
-
-  // Callback invoked when batch finishes.
-  function sendItemUpdateBatchNotification(record: BatchRecord) {
-    recordedBatches.push(record.events);
-  }
-
-  const firstBatch: Update[] = [
-    { id: "1", data: { backorderQuantity: 15 } },
-    { id: "1", data: { backorderQuantity: null } },
-  ];
-
-  const secondBatch: Update[] = [{ id: "1", data: { backorderQuantity: 12 } }];
-
-  for (const itemUpdate of firstBatch) {
-    notifyInBatch(itemUpdate.id, {
-      data: itemUpdate,
-      waitForMs: 100,
-      callback: sendItemUpdateBatchNotification,
-    });
-  }
-
-  assertEquals(recordedBatches.length, 0);
-
-  await delay(200);
-
-  for (const itemUpdate of secondBatch) {
-    notifyInBatch(itemUpdate.id, {
-      data: itemUpdate,
-      waitForMs: 100,
-      callback: sendItemUpdateBatchNotification,
-    });
-  }
-
-  assertEquals(recordedBatches.length, 1);
-  assertEquals(recordedBatches[0], firstBatch);
-
-  await delay(200);
-
-  const [firstBatchActual, secondBatchActual] = recordedBatches;
-  assertEquals(firstBatchActual, firstBatch);
-  assertEquals(secondBatchActual, secondBatch);
-});
