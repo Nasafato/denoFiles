397d7af76fef905e50d7466e3e87ab4b455aaa09
nasafato <alanjgou@gmail.com>
2020-08-19 18:33:46 -0700

    Use better API

--- batchNotifications.test.ts
+++ batchNotifications.test.ts
@@ -1,49 +1,72 @@
 import { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
 import { delay } from "https://deno.land/std@0.65.0/async/delay.ts";
 
-import { notifyInBatch, BatchRecord } from "./batchNotifications.ts";
+import { createBatchedFunction } from "./batchNotifications.ts";
 
-Deno.test("should be notified with two batches", async () => {
-  const recordedBatches: any[] = [];
+interface OrderItemUpdate {
+  id: string;
+  data: Record<string, any>;
+}
 
-  // Callback invoked when batch finishes.
-  function sendItemUpdateBatchNotification(record: BatchRecord) {
-    recordedBatches.push(record.events);
-  }
+Deno.test("should create two batches for order 1 and one batch for order 2", async () => {
+  const recordedBatches: Record<string, OrderItemUpdate[][]> = {};
 
-  const firstBatch = [
+  const dispatchUpdateEvent = createBatchedFunction<OrderItemUpdate>(
+    (batch) => {
+      if (!recordedBatches[batch.batchKey])
+        recordedBatches[batch.batchKey] = [];
+
+      recordedBatches[batch.batchKey].push(batch.events);
+    },
+    { waitForMs: 100 }
+  );
+
+  dispatchUpdateEvent("order1", { id: "1", data: { backorderQuantity: 15 } });
+  dispatchUpdateEvent("order2", { id: "2", data: { backorderQuantity: 11 } });
+
+  await delay(50);
+
+  dispatchUpdateEvent("order1", {
+    id: "1",
+    data: { backorderQuantity: null },
+  });
+
+  await delay(50);
+
+  // After 100 ms, order 2 batch should have expired, while order 1 hasn't
+  // because of the additional event fired after 50 ms.
+  assertEquals(recordedBatches.order1, undefined);
+  assertEquals(recordedBatches.order2.length, 1);
+  assertEquals(recordedBatches.order2[0], [
+    { id: "2", data: { backorderQuantity: 11 } },
+  ]);
+
+  await delay(50);
+
+  // after another 50 ms, first order 1 batch should have expired.
+  assertEquals(recordedBatches.order1.length, 1);
+  assertEquals(recordedBatches.order1[0], [
     { id: "1", data: { backorderQuantity: 15 } },
     { id: "1", data: { backorderQuantity: null } },
-  ];
+  ]);
 
-  const secondBatch = [{ id: "1", data: { backorderQuantity: 12 } }];
+  dispatchUpdateEvent("order1", { id: "1", data: { backorderQuantity: 12 } });
 
-  for (const itemUpdate of firstBatch) {
-    notifyInBatch(itemUpdate.id, {
-      data: itemUpdate,
-      waitForMs: 100,
-      callback: sendItemUpdateBatchNotification,
-    });
-  }
+  await delay(100);
 
-  assertEquals(recordedBatches.length, 0);
+  const { order1: order1Batches, order2: order2Batches } = recordedBatches;
 
-  await delay(200);
+  assertEquals(order1Batches.length, 2);
+  assertEquals(order1Batches[0], [
+    { id: "1", data: { backorderQuantity: 15 } },
+    { id: "1", data: { backorderQuantity: null } },
+  ]);
+  assertEquals(order1Batches[1], [
+    { id: "1", data: { backorderQuantity: 12 } },
+  ]);
 
-  for (const itemUpdate of secondBatch) {
-    notifyInBatch(itemUpdate.id, {
-      data: itemUpdate,
-      waitForMs: 100,
-      callback: sendItemUpdateBatchNotification,
-    });
-  }
-
-  assertEquals(recordedBatches.length, 1);
-  assertEquals(recordedBatches[0], firstBatch);
-
-  await delay(200);
-
-  const [firstBatchActual, secondBatchActual] = recordedBatches;
-  assertEquals(firstBatchActual, firstBatch);
-  assertEquals(secondBatchActual, secondBatch);
+  assertEquals(order2Batches.length, 1);
+  assertEquals(order2Batches[0], [
+    { id: "2", data: { backorderQuantity: 11 } },
+  ]);
 });
--- batchNotifications.ts
+++ batchNotifications.ts
@@ -1,44 +1,79 @@
-export interface BatchRecord {
+/*
+ * In-memory implementation of an event batcher.
+ *
+ * `createBatchedFunction` is basically a debounce function, except the function
+ * it creates records the data it's passed and will pass the recorded data to
+ * the function whenever the batch expires.
+ */
+
+export interface BatchRecord<DataType> {
   batchKey: string;
-  events: Record<string, any>[];
+  events: DataType[];
   timeout: number;
-  callback: BatchCompletionCallback;
+  callback: BatchCompletionCallback<DataType>;
 }
 
-export const batchStore: Record<string, BatchRecord | null> = {};
-export type BatchCompletionCallback = (record: BatchRecord) => void;
+/** Callback will be passed record with all batched events when batch times out. */
+export type BatchCompletionCallback<DataType> = (
+  batch: BatchRecord<DataType>
+) => void;
 
-export function notifyInBatch(
-  batchKey: string,
-  options: {
-    data: any;
-    waitForMs: number;
-    callback: BatchCompletionCallback;
-  }
+/** Each batch is keyed on an arbitrary string. */
+export const batchStore: Record<string, BatchRecord<any> | null> = {};
+
+interface EventBatchingOptions {
+  waitForMs: number;
+}
+
+/**
+ * Wraps a function and returns a debounced function, which will keep adding
+ * data to the batch when invoked within `waitForMs` milliseconds of the last
+ * invocation.
+ *
+ * The wrapped function will be called with the final result of the batching.
+ */
+export function createBatchedFunction<DataType = any>(
+  callback: BatchCompletionCallback<DataType>,
+  options: EventBatchingOptions
 ) {
-  const record = batchStore[batchKey];
-  if (!record) {
-    batchStore[batchKey] = {
-      batchKey,
-      events: [options.data],
-      callback: options.callback,
-      timeout: setBatchTrigger(batchKey, options.waitForMs),
-    };
+  /**
+   * When invoked, adds `data` to the batch record identified by the batch key.
+   * Sets off a timeout that will invoke the completion callback if this
+   * function isn't call on the same key within the `waitForMs` defined in
+   * `options`.
+   */
+  return function executeFunction(batchKey: string, data: DataType) {
+    const batch = batchStore[batchKey];
+    // If there's no existing record, create a new one.
+    if (!batch) {
+      batchStore[batchKey] = {
+        batchKey,
+        events: [data],
+        callback,
+        timeout: setTriggerBatchTimeout(batchKey, options.waitForMs),
+      };
 
-    return;
-  }
+      return;
+    }
 
-  clearTimeout(record.timeout);
-  record.events.push(options.data);
-  record.timeout = setBatchTrigger(batchKey, options.waitForMs);
+    // Otherwise, clear the previous timeout, add the newest event, and set a
+    // new timeout.
+    clearTimeout(batch.timeout);
+    batch.events.push(data);
+    batch.timeout = setTriggerBatchTimeout(batchKey, options.waitForMs);
+  };
 }
 
-function setBatchTrigger(batchKey: string, waitForMs: number) {
+/**
+ * For a given `batchKey`, wait for a certain amount of time before trying to
+ * invoke the batch's callback.
+ */
+function setTriggerBatchTimeout(batchKey: string, waitForMs: number) {
   return setTimeout(() => {
-    const record = batchStore[batchKey];
-    if (!record) throw new Error(`No record found on trigger for ${batchKey}`);
+    const batch = batchStore[batchKey];
+    if (!batch) throw new Error(`No batch found on trigger for ${batchKey}`);
 
-    record.callback(record);
+    batch.callback(batch);
     batchStore[batchKey] = null;
   }, waitForMs);
 }
